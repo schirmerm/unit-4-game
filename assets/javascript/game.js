19110acc7c38833f4e43887f283d9c87de977f2f
Maverick <maverick.schirmer@gmail.com>
2019-09-14 09:56:06 -0700

    Added click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,33 @@
-let randNumber = Math.floor(Math.random() * 120)
+let randNumber = Math.floor((Math.random() * 101) + 19);
+let score = 0;
+let wins = 0;
+let losses = 0;
+
+//Functiont to put randnumber in html
+const getNumber = _ => {
+    let randNumber = Math.floor((Math.random() * 101) + 19);
+    let getNumber = randNumber;
+    document.getElementById('randomNumber').innerHTML = `${getNumber}`
+}
+
+//Assign value to the gems
+const imageValue = _ => {
+    document.getElementById('gem1').src = Math.floor(Math.random()*12)
+}
+//Create Click functions for Gems
+const runGame = _ => {
+    document.getElementById('gem1').addEventListener('click', function(){
+        
+    })
+    document.getElementById('gem2').addEventListener('click', function(){
+        
+    })
+    document.getElementById('gem3').addEventListener('click', function(){
+        
+    })
+    document.getElementById('gem4').addEventListener('click', function(){
+        
+    })
+        
+}
+runGame()
